55a86430653a5f813427cc7cb8e4ca668d06c353
jennAllen <51343347+jennAllen@users.noreply.github.com>
2024-03-08 14:13:37 -0800

    Update app.py
    Updating branch 1

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from streamlit_player import st_player
 #st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")
 
 # Embed a youtube video - Alan Walker
-#st_player("https://www.youtube.com/watch?v=wJnBTPUQS5A")
+Video should be Alan Walker
+st_player("https://www.youtube.com/watch?v=wJnBTPUQS5A")
 
 # Embed a music from SoundCloud
-st_player("https://soundcloud.com/imaginedragons/demons")
+#st_player("https://soundcloud.com/imaginedragons/demons")
